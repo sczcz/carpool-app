0374151eaa18f2dd839e8dfaf3758f9222931770
emmaanord <128476191+emmaanord@users.noreply.github.com>
2024-10-09 12:09:57 +0200

    added a header

--- scout-app/src/App.js
+++ scout-app/src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
+import './App.css'; 
 import Login from './components/Login';
 import Register from './components/Register';
 import FetchUsers from './components/FetchUsers';
 import TestSession from './components/TestSession';
 import Logout from './components/Logout';
-
+import Header from './components/Header'; // Importera Header-komponenten
 
 const App = () => {
     return (
         <div>
+            <Header /> {/* Lägg till Header-komponenten här */}
             <Login />
             <Register />
             <FetchUsers />
---
+++ scout-app/src/components/Header.js
@@ -0,0 +1,13 @@
+// src/components/Header.js
+
+import React from 'react';
+
+const Header = () => {
+    return (
+        <header>
+            <h1>JONSTORPS KUSTSCOUTKÅR</h1>
+        </header>
+    );
+};
+
+export default Header;
